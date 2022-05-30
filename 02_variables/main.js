3341dcb5f6d7c499bf42207b93b57722cfa2ccb3
oleksandrc <oleksandr.cherednyk@nice.com>
2022-05-30 10:52:24 +0300

    variables

---
+++ 02_variables/main.js
@@ -0,0 +1,16 @@
+// <var> used in old browsers before 2015 year
+var a = 5;
+
+// <let> used in newer browsers from 2015 year
+let b = 6;
+
+// <const> used to declare value which will not change, browsers are from 2015 year
+const PI = 3.15
+// PI = 4; // error
+
+console.log(a + b);
+
+// variable without declaration are undefined
+
+let c;
+console.log(c);
