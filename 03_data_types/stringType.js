cc6363fcb00883ff9201443e2a32897f1d779191
oleksandrc <oleksandr.cherednyk@nice.com>
2022-05-30 21:54:10 +0300

    string object

---
+++ 03_data_types/stringType.js
@@ -0,0 +1,12 @@
+let stringOne = "string";
+let stringTwo = new String("string");
+let stringThree = new String("string");
+
+console.log(stringOne);
+console.log(stringTwo);
+console.log(stringThree);
+
+console.log(stringTwo == stringThree);
+console.log(stringTwo === stringThree);
+
+console.log(stringTwo == stringTwo);
