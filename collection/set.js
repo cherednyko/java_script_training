9cb97e7a611aab6d562d08734ba9c5835c002fa2
OCherednyk <cheredniko91@gmail.com>
2022-06-08 18:40:35 +0300

    collections

---
+++ collection/set.js
@@ -0,0 +1,26 @@
+function TestObject(name, count){
+    this.name = name;
+    this.count = count;
+}
+
+let item1 = new TestObject("first", 1);
+let item2 = new TestObject("second", 2);
+let itemDuplicate1 = new TestObject("duplicate", 0); //itemDuplicate1 is not equal to itemDuplicate2
+let itemDuplicate2 = new TestObject("duplicate", 0);
+let itemDuplicate3 = itemDuplicate2;
+
+let itemObject1 = { name: "first", count: 1 };
+let itemObject2 = { name: "first", count: 1 };
+
+let setContainer = new Set();
+setContainer.add(item1);
+setContainer.add(item2);
+setContainer.add(itemDuplicate1);
+setContainer.add(itemDuplicate2);
+setContainer.add(itemDuplicate3);
+setContainer.add(itemObject1);
+setContainer.add(itemObject2);
+
+for (const containerElement of setContainer) {
+    console.log(containerElement);
+}
